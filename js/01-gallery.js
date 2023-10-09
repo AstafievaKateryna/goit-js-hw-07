c02b0de9d16f71870dc430dcdc89c2211539e4bf
AstafievaKateryna <134802379+AstafievaKateryna@users.noreply.github.com>
2023-10-09 23:21:23 +0300

    1

---
+++ js/01-gallery.js
@@ -0,0 +1,37 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+// }
+
+const ul = document.querySelector('.gallery')
+const li = galleryItems.map(({original, preview, description}) =>
+`<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`
+).join('');
+ul.insertAdjacentHTML('beforeend', li);
+
+ul.addEventListener('click', openImg);
+function openImg(evt){
+    evt.preventDefault();
+if (evt.target.className !== 'gallery__image'){
+    return;
+}
+
+
+const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`)
+
+
+
+instance.show()
+}
