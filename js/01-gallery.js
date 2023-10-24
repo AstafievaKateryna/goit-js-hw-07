ca8e177dfb36bb6ecfe92c7884425225b2f324b8
AstafievaKateryna <134802379+AstafievaKateryna@users.noreply.github.com>
2023-10-24 22:49:52 +0300

    123

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -19,13 +19,6 @@ const li = galleryItems.map(({original, preview, description}) =>
 ).join('');
 ul.insertAdjacentHTML('beforeend', li);
 
-ul.addEventListener('click', openImg);
-function openImg(evt){
-    evt.preventDefault();
-if (evt.target.className !== 'gallery__image'){
-    return;
-}
-
 
 const instance = basicLightbox.create(`
     <img src="${evt.target.dataset.source}" width="800" height="600">
