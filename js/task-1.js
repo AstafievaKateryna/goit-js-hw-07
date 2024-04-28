0933ef4bf72571e7beab726fdeb48481416a9a2a
AstafievaKateryna <134802379+AstafievaKateryna@users.noreply.github.com>
2024-04-28 15:25:57 +0300

    1

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+const categories = document.querySelectorAll('li.item');
+console.log(`Number of categories: ${categories.length}`);
+categories.forEach(category => {
+    const title = category.querySelector('h2');
+    console.log(`Category: ${title.textContent}`);
+    const countItem = category.querySelectorAll('li');
+    console.log(`Elements:${countItem.length}`);
+});
